25f06064d415e94bd2df40f7c838c7eb11932c90
Darío <dariogarcigarci@gmail.com>
2022-06-09 17:12:52 +0200

    Sugerencias

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-por-pais',
@@ -12,6 +13,8 @@ export class PorPaisComponent implements OnInit {
   termino: string = '';
   hayError: boolean = false;
   paisesResultado: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
@@ -20,6 +23,7 @@ export class PorPaisComponent implements OnInit {
 
   buscar(termino: string) {
     this.hayError = false;
+    this.mostrarSugerencias = false;
     this.termino = termino;
     this.paisService.buscarPais(this.termino)
     .subscribe({
@@ -34,7 +38,22 @@ export class PorPaisComponent implements OnInit {
   }
 
   sugerencias(termino: string) {
+    this.mostrarSugerencias = true;
     this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarPais(termino)
+      .subscribe({
+        next: (paises) => {
+          this.paisesSugeridos = paises.splice(0,5);
+        },
+        error: (err) => {
+          this.paisesSugeridos = [];
+        }
+      });
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
     
   }
 
